b0c9592eb473329747e10872c0beac9546669694
Alternative RVB - home <alternative.rvb@gmail.com>
2021-10-23 15:43:17 +0200

    Ajout page - info fictif

---
+++ page-3.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require 'admin/inc/config.php'
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php
+    include SITE_ROOT . 'admin/inc/meta.php'
+    ?>
+    <title>Page 3</title>
+</head>
+
+
+<body>
+    <?php
+    include SITE_ROOT . 'admin/inc/header.php'
+    ?>
+    <main class="container p-1">
+        <div class="row">
+            <h1>Page 3</h1>
+        </div>
+
+    </main>
+    <?php
+    include SITE_ROOT . "admin/inc/footer.php"
+    ?>
+</body>
+
+</html>
