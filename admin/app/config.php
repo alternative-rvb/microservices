4e747ff3f010262eb037752dfc41374cc3954405
Alternative RVB - home <alternative.rvb@gmail.com>
2021-12-23 22:26:15 +0100

    MAJ mineure

--- admin/app/config.php
+++ admin/app/config.php
@@ -1,14 +1,13 @@
-<?php 
+<?php
 // var_dump($_SERVER);
 
 // $project = '/microservices/';
 $project = '/';
 
-define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].$project);
+define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . $project);
 // var_dump(SITE_ROOT);
 
 define('WEB_ROOT', $project);
 
-define('IMAGES_ROOT', $project. 'uploads/images/');
+define('IMAGES_ROOT', $project . 'uploads/images/');
 // var_dump(IMAGES_ROOT);
-
